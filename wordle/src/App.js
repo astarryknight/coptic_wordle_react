e9d65f9c16acbe243799f4002acde175272a1e3f
Starry Knight <whiteh4tter@gmail.com>
2024-08-14 08:19:06 -0400

    cleaned up some code

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -24,7 +24,7 @@ function submitGuess() {
 
 }
 
-function Row({ rowNum, guess }) {
+function Row({ rowNum, guess, guesses }) {
   var n = parseInt(rowNum)
   if (n == currentRow) {
     return (
@@ -34,6 +34,16 @@ function Row({ rowNum, guess }) {
         <Box className="box">{guess[2]}</Box>
         <Box className="box">{guess[3]}</Box>
         <Box className="box">{guess[4]}</Box>
+      </Stack>
+    )
+  } else if (n < currentRow) {
+    return (
+      <Stack direction="row" spacing={.75}>
+        <Box className="box">{guesses[n][0]}</Box>
+        <Box className="box">{guesses[n][1]}</Box>
+        <Box className="box">{guesses[n][2]}</Box>
+        <Box className="box">{guesses[n][3]}</Box>
+        <Box className="box">{guesses[n][4]}</Box>
       </Stack>
     )
   } else {
@@ -47,88 +57,69 @@ function Row({ rowNum, guess }) {
       </Stack>
     )
   }
-
 }
+
+function Key({ l, guess, setGuess }) {
+  return (
+    <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={() => { ({ guess }.guess.length <= 4 && setGuess({ guess }.guess + l)) }}>{l.toUpperCase()}</Button>
+  )
+}
+
+function gameOver() {
+  //the game ends...
+}
+
 
 function App() {
 
-  const [guess, setGuess] = React.useState("h")
+  const [guess, setGuess] = React.useState("")
+  const [guesses, setGuesses] = React.useState([]);
 
   return (
     <Stack direction="column" justifyContent="center" alignItems="center" spacing={3} sx={{ height: "100%" }}>
       <Typography>Hi</Typography>
       <Stack direction="column" spacing={.75}>
-        <Row rowNum="0" guess={guess} />
-        <Stack direction="row" spacing={.75}>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-        </Stack>
-        <Stack direction="row" spacing={.75}>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-        </Stack>
-        <Stack direction="row" spacing={.75}>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-        </Stack>
-        <Stack direction="row" spacing={.75}>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-        </Stack>
-        <Stack direction="row" spacing={.75}>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-          <Box className="box"></Box>
-        </Stack>
+        <Row rowNum="0" guess={guess} guesses={guesses} />
+        <Row rowNum="1" guess={guess} guesses={guesses} />
+        <Row rowNum="2" guess={guess} guesses={guesses} />
+        <Row rowNum="3" guess={guess} guesses={guesses} />
+        <Row rowNum="4" guess={guess} guesses={guesses} />
+        <Row rowNum="5" guess={guess} guesses={guesses} />
       </Stack>
       <Stack direction="column" alignItems="center" spacing={.7}>
         <Stack direction="row" spacing={.7} sx={{ height: "3.5em" }}>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={() => { console.log({ guess }.guess) }}>Q</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("w") }}>W</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("e") }}>E</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("r") }}>R</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("t") }}>T</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("y") }}>Y</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("u") }}>U</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("i") }}>I</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("o") }}>O</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("p") }}>P</Button>
+          <Key l="q" guess={guess} setGuess={setGuess} />
+          <Key l="w" guess={guess} setGuess={setGuess} />
+          <Key l="e" guess={guess} setGuess={setGuess} />
+          <Key l="r" guess={guess} setGuess={setGuess} />
+          <Key l="t" guess={guess} setGuess={setGuess} />
+          <Key l="y" guess={guess} setGuess={setGuess} />
+          <Key l="u" guess={guess} setGuess={setGuess} />
+          <Key l="i" guess={guess} setGuess={setGuess} />
+          <Key l="o" guess={guess} setGuess={setGuess} />
+          <Key l="p" guess={guess} setGuess={setGuess} />
         </Stack>
         <Stack direction="row" spacing={.7} sx={{ height: "3.5em" }}>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("a") }}>A</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("s") }}>S</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("d") }}>D</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("f") }}>F</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("g") }}>G</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("h") }}>H</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("j") }}>J</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("k") }}>K</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("l") }}>L</Button>
+          <Key l="a" guess={guess} setGuess={setGuess} />
+          <Key l="s" guess={guess} setGuess={setGuess} />
+          <Key l="d" guess={guess} setGuess={setGuess} />
+          <Key l="f" guess={guess} setGuess={setGuess} />
+          <Key l="g" guess={guess} setGuess={setGuess} />
+          <Key l="h" guess={guess} setGuess={setGuess} />
+          <Key l="j" guess={guess} setGuess={setGuess} />
+          <Key l="k" guess={guess} setGuess={setGuess} />
+          <Key l="l" guess={guess} setGuess={setGuess} />
         </Stack>
         <Stack direction="row" spacing={.7} sx={{ height: "3.5em" }}>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { submitGuess() }}>Enter</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("z") }}>Z</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("x") }}>X</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("c") }}>C</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("v") }}>V</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("b") }}>B</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("n") }}>N</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { addLetter("m") }}>M</Button>
-          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { removeLetter({ guess }) }}>Bksp</Button>
+          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { ({ guess }.guess == target || currentRow == 5 ? gameOver() : (currentRow++)) }}>Enter</Button>
+          <Key l="z" guess={guess} setGuess={setGuess} />
+          <Key l="x" guess={guess} setGuess={setGuess} />
+          <Key l="c" guess={guess} setGuess={setGuess} />
+          <Key l="v" guess={guess} setGuess={setGuess} />
+          <Key l="b" guess={guess} setGuess={setGuess} />
+          <Key l="n" guess={guess} setGuess={setGuess} />
+          <Key l="m" guess={guess} setGuess={setGuess} />
+          <Button sx={{ backgroundColor: "#d3d6da", color: "black", fontWeight: "bold" }} onClick={function () { ({ guess }.guess.length >= 0 && setGuess({ guess }.guess.substring(0, { guess }.guess.length - 1))) }}>Bksp</Button>
         </Stack>
       </Stack>
     </Stack >
