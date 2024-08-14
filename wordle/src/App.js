5cbd326c386e7301581b2f9d099281fdb0abf997
Starry Knight <whiteh4tter@gmail.com>
2024-08-14 07:11:27 -0400

    init react and joyui

---
+++ wordle/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Stack from '@mui/joy/Stack';
+import Box from '@mui/joy/Stack';
+
+function App() {
+  return (
+    <Stack>
+      <Stack>
+        <Box className="box"></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Stack>
+      <Stack></Stack>
+      <Stack></Stack>
+      <Stack></Stack>
+      <Stack></Stack>
+      <Stack></Stack>
+    </Stack>
+  );
+}
+
+export default App;
